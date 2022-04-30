80d396d24b0f3da2699b6bcdbc1bf66d7be8d86d
greenbender <byron.platt@gmail.com>
2022-04-30 17:18:23 +1000

    Update and cleanup code
    Changes:
     - Cleanup code formatting
     - Fix broken round to half implemenation
     - Simplify the way values in attrs and styles are rounded

--- jtx.rounder/rounder.py
+++ jtx.rounder/rounder.py
@@ -20,175 +20,129 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 '''
-import inkex, re
+import inkex
 
 
 class svgRounder(inkex.Effect):
     def __init__(self):
-        inkex.Effect.__init__(self)
-        self.arg_parser.add_argument("--p","--precision",
-                             type=int,
-                             dest="precision", default=3,
-                             help="Precision")
+        super().__init__()
+        self.arg_parser.add_argument("--p", "--precision",
+            type=int, dest="precision", default=3,
+            help="Precision")
         self.arg_parser.add_argument("-c", "--ctrl",
-                        type=inkex.Boolean,
-                        dest="ctrl", default = False,
-                        help="Round node handles")
+            type=inkex.Boolean, dest="ctrl", default=False,
+            help="Round node handles")
         self.arg_parser.add_argument("-a", "--along",
-                        type=inkex.Boolean,
-                        dest="along", default = True,
-                        help="Move handles following node movement")
+            type=inkex.Boolean, dest="along", default=True,
+            help="Move handles following node movement")
         self.arg_parser.add_argument("--half",
-                        type=inkex.Boolean,
-                        dest="half", default = False,
-                        help="Allow round to half if nearest")
+            type=inkex.Boolean, dest="half", default=False,
+            help="Allow round to half if nearest")
         self.arg_parser.add_argument("-p", "--paths",
-                        type=inkex.Boolean,
-                        dest="paths", default = True,
-                        help="Affect to paths")
+            type=inkex.Boolean, dest="paths", default=True,
+            help="Affect to paths")
         self.arg_parser.add_argument("-w", "--widthheight",
-                        type=inkex.Boolean,
-                        dest="widthheight", default = False,
-                        help="Affect to width and height of objects")
+            type=inkex.Boolean, dest="widthheight", default=False,
+            help="Affect to width and height of objects")
         self.arg_parser.add_argument("-x", "--position",
-                        type=inkex.Boolean,
-                        dest="position", default = False,
-                        help="Affect to position of objects")
+            type=inkex.Boolean, dest="position", default=False,
+            help="Affect to position of objects")
         self.arg_parser.add_argument("-s", "--strokewidth",
-                        type=inkex.Boolean,
-                        dest="strokewidth", default = False,
-                        help="Affect to stroke width of objects")
+            type=inkex.Boolean, dest="strokewidth", default=False,
+            help="Affect to stroke width of objects")
         self.arg_parser.add_argument("-o", "--opacity",
-                        type=inkex.Boolean,
-                        dest="opacity", default = False,
-                        help="Affect to global opacity of objects")
+            type=inkex.Boolean, dest="opacity", default=False,
+            help="Affect to global opacity of objects")
         self.arg_parser.add_argument("--strokeopacity",
-                        type=inkex.Boolean,
-                        dest="strokeopacity", default = False,
-                        help="Affect to stroke opcacity of objects")
+            type=inkex.Boolean, dest="strokeopacity", default=False,
+            help="Affect to stroke opcacity of objects")
         self.arg_parser.add_argument("--fillopacity",
-                        type=inkex.Boolean,
-                        dest="fillopacity", default = False,
-                        help="Affect to fill opcacity of objects")
+            type=inkex.Boolean, dest="fillopacity", default=False,
+            help="Affect to fill opcacity of objects")
 
     def roundFloat(self, n):
         if self.options.half:
-            if self.options.precision == 0:
-                return str(round(n * 2) / 2)
-            else:
-                return str(round(n * (self.options.precision) * 10 * 2) / ((self.options.precision) * 10 * 2))
-        else:
-            return str(round(n,  self.options.precision))
+            multiplier = 10 ** self.options.precision
+            n *= multiplier
+            n = round(n * 2) / 2
+            return n / multiplier
+        return round(n,  self.options.precision)
  
-    def roundit(self, p):
+    def roundUnit(self, value):
+        value, unit = inkex.units.parse_unit(value, '')
+        value = self.roundFloat(value)
+        return f'{value}{unit}'
+        
+    def roundPoint(self, p):
         x = self.roundFloat(p[0])
         y = self.roundFloat(p[1])
-        return [float(x) - p[0], float(y) - p[1]]
+        return [x - p[0], y - p[1]]
     
-    def path_round_it(self,node):
-        if node.tag == inkex.addNS('path','svg'):
+    def path_round_it(self, node):
+        if node.tag == inkex.addNS('g', 'svg'):
+            for e in node:
+                self.path_round_it(e)
+        elif node.tag == inkex.addNS('path', 'svg'):
             d = node.get('d')
             p = inkex.CubicSuperPath(d)
             for subpath in p:
                 for csp in subpath:
-                    delta = self.roundit(csp[1])
+                    delta = self.roundPoint(csp[1])
                     if self.options.along:
-                        csp[0][0]+=delta[0] 
-                        csp[0][1]+=delta[1] 
-                    csp[1][0]+=delta[0] 
-                    csp[1][1]+=delta[1] 
+                        csp[0][0] += delta[0] 
+                        csp[0][1] += delta[1] 
+                    csp[1][0] += delta[0] 
+                    csp[1][1] += delta[1] 
                     if self.options.along:
-                        csp[2][0]+=delta[0] 
-                        csp[2][1]+=delta[1] 
+                        csp[2][0] += delta[0] 
+                        csp[2][1] += delta[1] 
                     if self.options.ctrl:
-                        delta = self.roundit(csp[0])
-                        csp[0][0]+=delta[0] 
-                        csp[0][1]+=delta[1] 
-                        delta = self.roundit(csp[2])
-                        csp[2][0]+=delta[0] 
-                        csp[2][1]+=delta[1] 
+                        delta = self.roundPoint(csp[0])
+                        csp[0][0] += delta[0] 
+                        csp[0][1] += delta[1] 
+                        delta = self.roundPoint(csp[2])
+                        csp[2][0] += delta[0] 
+                        csp[2][1] += delta[1] 
             node.set('d', p.to_path())
-        elif node.tag == inkex.addNS('g','svg'):
+
+    def style_round_it(self, node, name):
+        if node.tag == inkex.addNS('g', 'svg'):
             for e in node:
-                self.path_round_it(e)
+                self.style_round_it(e)
+        elif name in node.style:
+            node.style[name] = self.roundUnit(node.style[name])
 
-    def roundStroke(self,matchobj):
-        return 'stroke-width:' + self.roundFloat(float( matchobj.group(1))) +  matchobj.group(2);
-
-    def roundOpacity(self,matchobj):
-        return matchobj.group(1) + matchobj.group(2) + self.roundFloat(float( matchobj.group(3))) +  matchobj.group(4);
-
-    def roundWHXY(self,matchobj):
-        return matchobj.group(1) + self.roundFloat(float( matchobj.group(2))) +  matchobj.group(3);
-
-
-    def stroke_round_it(self, node):
-        if node.tag == inkex.addNS('g','svg'):
+    def attribute_round_it(self, node, name):
+        if node.tag == inkex.addNS('g', 'svg'):
             for e in node:
-                self.stroke_round_it(e)
+                self.attribute_round_it(e)
         else:
-            style = node.get('style')
-            if style:
-                style = re.sub('stroke-width:(.*?)(;|$)',self.roundStroke, style)
-                node.set('style',style)
-    def opacity_round_it(self, node, typeOpacity):
-        if node.tag == inkex.addNS('g','svg'):
-            for e in node:
-                self.opacity_round_it(e)
-        else:
-            style = node.get('style')
-            if style:
-                style = re.sub('(^|;)(' + typeOpacity + ':)(.*?)(;|$)',self.roundOpacity, style)
-                node.set('style',style)
+            value = node.get(name)
+            if value:
+                node.set(name, self.roundUnit(value))
 
-    def widthheight_round_it(self, node):
-        if node.tag == inkex.addNS('g','svg'):
-            for e in node:
-                self.widthheight_round_it(e)
-        else:
-            width = node.get('width')
-            if width:
-                width = re.sub('^(\-|)([0-9]+\.[0-9]+)(.*?)$',self.roundWHXY, width)
-                node.set('width',width)
-            height = node.get('height')
-            if height:
-                height = re.sub('^(\-|)([0-9]+\.[0-9]+)(.*?)$',self.roundWHXY, height)
-                node.set('height',height)
-    
-    def position_round_it(self, node):
-        if node.tag == inkex.addNS('g','svg'):
-            for e in node:
-                self.position_round_it(e)
-        else:
-            x = node.get('x')
-            if x:
-                x = re.sub('^(\-|)([0-9]+\.[0-9]+)(.*?)$',self.roundWHXY, x)
-                node.set('x', x)
-            y = node.get('y')
-            if y:
-                y = re.sub('^(\-|)([0-9]+\.[0-9]+)(.*?)$',self.roundWHXY, y)
-                node.set('y', y)
-    
     def effect(self):
         for node in self.svg.selected:
             if self.options.paths:
                 self.path_round_it(node)
             if self.options.strokewidth:
-                self.stroke_round_it(node)
+                self.style_round_it(node, 'stroke-width')
             if self.options.widthheight:
-                self.widthheight_round_it(node)
+                self.attribute_round_it(node, 'width')
+                self.attribute_round_it(node, 'height')
             if self.options.position:
-                self.position_round_it(node)
+                self.attribute_round_it(node, 'x')
+                self.attribute_round_it(node, 'y')
             if self.options.opacity:
-                self.opacity_round_it(node, "opacity")
+                self.style_round_it(node, 'opacity')
             if self.options.strokeopacity:
-                self.opacity_round_it(node, "stroke-opacity")
+                self.style_round_it(node, 'stroke-opacity')
             if self.options.fillopacity:
-                self.opacity_round_it(node, "fill-opacity")
+                self.style_round_it(node, 'fill-opacity')
+
 
 if __name__ == '__main__':
-    e = svgRounder()
-    e.run()
+    svgRounder().run()
 
 
 # vim: expandtab shiftwidth=4 tabstop=8 softtabstop=4 fileencoding=utf-8 textwidth=99
